383ff1ec2dcb4e689b0b4af099414871c3f8c142
bin lu <lu.bin.lu.bin@gmail.com>
2017-07-28 10:53:59 -0400

    chapter 6 flickr init commit

---
+++ chapter6/flickr.js
@@ -0,0 +1,54 @@
+requirejs.config({
+  paths: {
+    ramda: 'https://cdnjs.cloudflare.com/ajax/libs/ramda/0.13.0/ramda.min',
+    jquery: 'https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min'
+  },
+});
+
+require([
+    'ramda',
+    'jquery',
+  ],
+  function(_, $) {
+    var trace = _.curry(function(tag, x) {
+      console.log(tag, x);
+      return x;
+    });
+    // app goes here
+
+    var Impure = {
+      getJSON: _.curry(function(callback, url) {
+        $.getJSON(url, callback);
+      }),
+
+      setHtml: _.curry(function(sel, html) {
+        $(sel).html(html);
+      })
+    };
+
+    var url = function(term) {
+      return 'https://api.flickr.com/services/feeds/photos_public.gne?tags=' +
+      term + '&format=json&jsoncallback=?';
+    };
+
+    var prop = _.curry(function(property, object) {
+      return object[property];
+    });
+
+    var mediaUrl = _.compose(_.prop('m'), _.prop('media'));
+
+    var srcs = _.compose(_.map(mediaUrl), _.prop('items'));
+
+    var img = function(url) {
+      return $('<img />', {
+        src: url
+      });
+    };
+
+    var images = _.compose(_.map(img), srcs);
+    
+    var renderImages = _.compose(Impure.setHtml('body'), trace('srcs'), images);
+    var app = _.compose(Impure.getJSON(renderImages), url);
+
+    app('cats');
+  });
